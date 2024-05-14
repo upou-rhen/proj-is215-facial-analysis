dd5c16c41fcce67e40db7796575957abd28a7bf5
Rhen Mark <rhenmarkc@gmail.com>
2024-05-15 02:28:11 +0800

    chore: initial page

--- app/fotoscope/src/app/page.tsx
+++ app/fotoscope/src/app/page.tsx
@@ -1,34 +1,45 @@
-'use client'
+"use client";
 
 import Image from "next/image";
 import { useState } from "react";
-import Dropzone, { useDropzone } from 'react-dropzone'
+import { useDropzone } from "react-dropzone";
+import AWS from "aws-sdk";
+import S3 from "aws-sdk/clients/s3"; //
 
 export default function Home() {
   const [files, setFiles] = useState<any>([]);
-  const [isGenerating, setIsGenerating] = useState<boolean>(false)
+  const [isGenerating, setIsGenerating] = useState<boolean>(false);
+  const { uploadFile } = useGenerate();
   const {
     // acceptedFiles,
     getRootProps,
-    getInputProps
+    getInputProps,
   } = useDropzone({
     accept: {
-      'image/jpeg': [],
-      'image/png': []
+      "image/jpeg": [],
+      "image/png": [],
     },
     maxFiles: 1,
     onDrop(acceptedFiles, fileRejections, event) {
-      setFiles(acceptedFiles)
+      setFiles(acceptedFiles);
     },
   });
 
   const handleClear = () => {
-    setFiles([])
-  }
+    setFiles([]);
+  };
 
-  const handleGenerate = () => {
-    setIsGenerating(true)
-  }
+  const handleGenerate = async () => {
+    setIsGenerating(true);
+    const { data, error } = await uploadFile(files[0]);
+
+    if (error) {
+      setIsGenerating(false);
+    }
+
+    // handle data here
+    console.log("data ==>", data);
+  };
 
 
   return (
@@ -37,39 +48,112 @@ export default function Home() {
         <h4 className="text-black text-2xl">Fotoscope</h4>
       </header>
       <section className="grid grid-flow-col justify-center items-start mt-20 gap-10">
-        <div {...getRootProps({ className: 'dropzone' })} className="border border-dashed min-h-[400px] grid place-items-center p-10 bg-slate-100/10">
+        <div
+          {...getRootProps({ className: "dropzone" })}
+          className="border border-dashed min-h-[400px] grid place-items-center p-10 bg-slate-100/10"
+        >
           <div>
             <input {...getInputProps()} />
             <p>Drag {"'n'"} drop some files here, or click to select files</p>
             <em>(1 file and only *.jpeg and *.png images will be accepted)</em>
           </div>
         </div>
-        {
-          files.length ? <div>
+        {files.length ? (
+          <div>
             {files.map((item: any, index: string) => {
-              const url = URL.createObjectURL(item)
+              const url = URL.createObjectURL(item);
               return (
-                <Image src={url} alt="selected" key={index} width={600} height={600} />
-              )
+                <Image
+                  src={url}
+                  alt="selected"
+                  key={index}
+                  width={600}
+                  height={600}
+                />
+              );
             })}
             <div className="grid grid-flow-col w-full justify-end gap-4 mt-4">
-              <button disabled={isGenerating} type="button" className="h-8 p-2 px-4 grid place-items-center box-content bg-slate-500 disabled:bg-slate-500/45 rounded-sm min-w-[100px]" onClick={handleClear}>Clear</button>
-              <button disabled={isGenerating} type="button" className="h-8 p-2 px-4 grid grid-flow-col gap-2 place-items-center box-content bg-blue-600 disabled:bg-blue-600/45 rounded-sm" onClick={handleGenerate}>
+              <button
+                disabled={isGenerating}
+                type="button"
+                className="h-8 p-2 px-4 grid place-items-center box-content bg-slate-500 disabled:bg-slate-500/45 rounded-sm min-w-[100px]"
+                onClick={handleClear}
+              >
+                Clear
+              </button>
+              <button
+                disabled={isGenerating}
+                type="button"
+                className="h-8 p-2 px-4 grid grid-flow-col gap-2 place-items-center box-content bg-blue-600 disabled:bg-blue-600/45 rounded-sm"
+                onClick={handleGenerate}
+              >
                 {isGenerating ? <Loading /> : null}
                 Generate news
               </button>
             </div>
-          </div> : null
-        }
-
+          </div>
+        ) : null}
       </section>
     </main>
   );
 }
 
-
 const Loading = () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" className="animate-spin" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M440-122q-121-15-200.5-105.5T160-440q0-66 26-126.5T260-672l57 57q-38 34-57.5 79T240-440q0 88 56 155.5T440-202v80Zm80 0v-80q87-16 143.5-83T720-440q0-100-70-170t-170-70h-3l44 44-56 56-140-140 140-140 56 56-44 44h3q134 0 227 93t93 227q0 121-79.5 211.5T520-122Z" /></svg>
-  )
-}
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="animate-spin"
+      height="24px"
+      viewBox="0 -960 960 960"
+      width="24px"
+      fill="#e8eaed"
+    >
+      <path d="M440-122q-121-15-200.5-105.5T160-440q0-66 26-126.5T260-672l57 57q-38 34-57.5 79T240-440q0 88 56 155.5T440-202v80Zm80 0v-80q87-16 143.5-83T720-440q0-100-70-170t-170-70h-3l44 44-56 56-140-140 140-140 56 56-44 44h3q134 0 227 93t93 227q0 121-79.5 211.5T520-122Z" />
+    </svg>
+  );
+};
+
+const useGenerate = () => {
+  const bucketName = "is215-a5-rmcallado";
+  const bucketUrl = "https://" + bucketName + ".s3.amazonaws.com/";
+
+  const S3_BUCKET = bucketName; // Replace with your bucket name
+  const REGION = "us-east-1"; // Replace with your region
+
+  const uploadFile = async (file: any) => {
+    AWS.config.update({
+      accessKeyId: "your_accesskeyID",
+      secretAccessKey: "your_secretAccessKey",
+    });
+
+    const s3 = new S3({
+      params: { Bucket: S3_BUCKET },
+      region: REGION,
+    });
+
+    const params = {
+      Bucket: S3_BUCKET,
+      Key: file.name,
+      Body: file,
+    };
+
+    try {
+      const upload = await s3.putObject(params).promise();
+      console.log("upload ==>", upload);
+      return {
+        error: false,
+        data: {},
+      };
+    } catch (error) {
+      console.error(error);
+      return {
+        error: true,
+        data: {},
+      };
+    }
+  };
+
+  return {
+    uploadFile,
+  };
+};
